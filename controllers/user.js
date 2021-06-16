8736bdc47403e77e6ca7176ccd8edf58bbef306d
faithdinno <adebayooluwadara@gmail.com>
2021-06-16 23:06:53 +0100

    updates

--- controllers/user.js
+++ controllers/user.js
@@ -243,3 +243,73 @@ exports.updatePassword = async(req, res) => {
         }
     }
 }
+
+//CHANGE PASSWORD
+exports.changePassword = async(req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        res.status(422).json({
+            message: "validation error",
+            error: errors.mapped()
+        })
+    } else {
+        try {
+            bcrypt.genSalt(saltRounds, function(err, salt) {
+                bcrypt.hash(req.body.newPassword, salt, function(err, hash) {
+                    if (err) {
+                        res.status(403).json({
+                            message: "An error occur",
+                            response: err
+                        })
+                    } else {
+                        const QUERY = `SELECT * FROM ${SCHEMA}.${TABLE} WHERE id="${req.params.id}"`;
+                        db.query(QUERY)
+                            .then(user => {
+                                if (user.data != null) {
+                                    bcrypt.compare(req.body.currentPassword, user.data[0].password.hash, function(err, result) {
+                                        if (result) {
+                                            db.update({
+                                                    table: TABLE,
+                                                    records: [{
+                                                        id: req.params.id,
+                                                        password: { salt: salt, hash: hash },
+                                                    }, ],
+                                                })
+                                                .then(result => {
+                                                    res.status(200).json({
+                                                        message: "Password Changed Successfully",
+                                                        response: result
+                                                    });
+                                                })
+                                                .catch(error => {
+                                                    res.status(422).json({
+                                                        message: "An error occur",
+                                                        response: error
+                                                    });
+                                                })
+                                        } else {
+                                            res.status(400).json({
+                                                message: "Current Password is wrong",
+                                                response: err
+                                            })
+                                        }
+                                    })
+                                }
+                            })
+                            .catch(error => {
+                                res.status(422).json({
+                                    message: "An error occur, unable to fetch user data with the id given",
+                                    response: error
+                                });
+                            })
+                    }
+                });
+            });
+        } catch (error) {
+            res.status(422).json({
+                message: "An error occur",
+                response: error
+            });
+        }
+    }
+}
